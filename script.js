13fc53bcc4b2f4e489d5b88ce6e6166afc50f9ac
Jure Cuhalev <gandalfar@gmail.com>
2011-12-03 21:56:18 +0100

    initial commit at the end of #ohdh

---
+++ script.js
@@ -0,0 +1,124 @@
+$(document).ready(function() {
+
+    function convertDate(dateArray) {
+        y = dateArray[0]; m = dateArray[1]-1; d = dateArray[2];
+
+        if (y === 999) {
+            return new Date();
+        };
+        return new Date(y, m, d)
+    }
+
+    var data = []
+    var stranke = {}
+
+    d3.json("stranke.json", function(json){
+        var obj = json.stranke_condensed;
+        for(var prop in obj) {
+            if(obj.hasOwnProperty(prop))
+                data.push(obj[prop])
+        }
+    
+        var width = 1200;
+        var height = 400;
+        var padding = 40;
+
+        var x = d3.time.scale().domain([new Date(1989, 0, 1), new Date(2011, 11, 31)]).range([0, width]);
+        var y = d3.scale.linear().domain([0, 22]).range([0, height])
+
+        var timeline = d3.select("#timeline")
+            .append('svg:svg')
+            .attr('class', 'timeline')
+            .attr('width', width+padding)
+            .attr('height', height+padding)
+
+        var axisGroup = timeline.append("svg:g")
+            .attr("transform", "translate("+padding+","+padding+")")
+
+        axisGroup.selectAll("line")
+             .data(x.ticks(10))
+           .enter().append("svg:line")
+             .attr("x1", x)
+             .attr("x2", x)
+             .attr("y1", 0)
+             .attr("y2", height)
+             // .attr("stroke", "#ccc");
+        
+        // draw political parties
+        var line_counter = 0;
+        data.map(function(stranka, i){
+            line_counter += 1;
+            
+
+            var sg = timeline.append("svg:g")
+                .attr("transform", "translate("+padding+","+padding+")")
+                //.attr("id", "group_"+line_counter)
+            
+            sg.selectAll("line")
+                .data(stranka)
+                .enter().append("svg:line")
+                    .attr("x1", function(d) { return x(convertDate(d.od)) })
+                    .attr("x2", function(d) { return x(convertDate(d.do)) })
+                    .attr("y1", function()  { d.line = line_counter; return y(line_counter); })
+                    .attr("y2", function()  { return y(line_counter); })
+                    .attr("stroke", "#ccc")
+                    .attr("stroke-width", '3')
+                    .attr("id", function(d) { return "stranka_"+d.id; })
+                    .on('mouseover', function(d,i){
+                        $('#title').html(d.ime);
+                        $(this).attr('stroke-width', '5').attr("stroke", 'red');
+                    })
+                    .on('mouseout', function(d,i){
+                        console.log('leave');
+                        $(this).attr('stroke-width', '3').attr("stroke", '#ccc');
+                    })
+
+        })
+
+        // draw connections
+        data.map(function(stranka, index){
+            /* copy all parties into new array and remove the ones that have
+               empty 'nastala_iz' since there is nothing to connect them to
+               (I don't know yet how to do this directly from d3.data() loop) */
+            var povezave = stranka.slice();
+            for (var i = povezave.length - 1; i >= 0; i--) {
+                if (povezave[i].nastala_iz.length === 0) {
+                    povezave.splice(i,1);
+                };
+            };
+            
+            //var sg = d3.select('#stranka_'+(index+1));
+
+            var sg = timeline.append("svg:g")
+                .attr("transform", "translate("+padding+","+padding+")")
+
+            povezave.map(function(pov){
+                sg.selectAll("line")
+                .data(pov.nastala_iz)
+                .enter().append("svg:line")
+                    .attr("x1", function(d, i) { 
+                        return $('#stranka_'+d).attr('x2');
+                    })
+                    .attr("y1", function(d, i) {
+                        return $('#stranka_'+d).attr('y2');  
+                    })
+                    .attr("x2", function(d, i) { 
+                        pov_x1 = parseFloat($('#stranka_'+pov.id).attr('x1'));
+                        d_x1   = parseFloat($('#stranka_'+d).attr('x2'));
+
+                        return Math.max(pov_x1, d_x1);
+                    })
+                    .attr("y2", function(d, i) { 
+                        return $('#stranka_'+pov.id).attr('y1');
+                    })
+                    .attr("stroke", "#ccc")
+                    .attr("id", function(d) { return "stranka_"+d.id; })
+
+            });
+
+        
+        })
+
+        
+    })
+});
